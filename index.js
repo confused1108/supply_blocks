7a786e5efe39f5029fc40927be2b3774abfe4973
anjali-jain0 <jainanjali043@gmail.com>
2019-06-03 11:38:41 +0530

    fix index.js

--- index.js
+++ index.js
@@ -36,25 +36,56 @@ const IndSchema = new mongoose.Schema({
 	password : {type : String , required : true}
 });
 
+const TransportersSchema = new mongoose.Schema({
+	name : {type : String , required : true},
+	from_place : {type : String , required : true},
+	to_place : {type : String , required : true},
+	obtain_from : {type : String , required : true},
+	give_to : {type : String , required : true}
+});
+
+const OrderSchema = new mongoose.Schema({
+	order_id : {type : Number , required : true}
+	seller_detail : {type : String , required : true},
+	buyer_detail : {type : String , required : true},
+	product_details : {type : Object , required : true},
+    transporter_details : TransportersSchema
+});
+
+const NodeSchema = new mongoose.Schema({
+	name : {type : String , required : true},
+	number : {type : Number , required : true},
+	email : {type : String , required : true}
+});
+
+
 CompanySchema.plugin(timestamp);
 IndSchema.plugin(timestamp);
 PackageSchema.plugin(timestamp);
+TransportersSchema.plugin(timestamp);
+OrderSchema.plugin(timestamp);
+NodeSchema.plugin(timestamp);
 
+const Node = mongoose.model('Node', NodeSchema , 'Node');
+const Order = mongoose.model('Order', OrderSchema , 'Order');
 const Company = mongoose.model('Company', CompanySchema , 'Company');
 const Ind = mongoose.model('Ind', IndSchema , 'Ind');
 const Package = mongoose.model('Package', PackageSchema , 'Package');
 
+//Company registration
 app.post('/user/:cname/:website/:email/:number/:password',function(req,res){
 	const {cname , website , email , number , password} = req.params ;  
 	const company = Company({cname , website , email , number , password}).save(err => console.log(err));
 });
 
+//Individual user registration
 app.post('/induser/:firstname/:lastname/:email/:number/:password',function(req,res){
     
 	const {firstname , lastname , email , number , password} = req.params ;  
 	const ind = Ind({firstname , lastname , email , number , password}).save(err => console.log(err));
 });
 
+//Company login
 app.get('/searchuser/:email/:password',function(req,res){
    
 	const {email , password} = req.params ; 
@@ -66,6 +97,7 @@ app.get('/searchuser/:email/:password',function(req,res){
 	});
 });
 
+//Individual user login
 app.post('/searchinduser/:email/:password',function(req,res){
 
 	const {email , password} = req.params ; 
@@ -76,6 +108,7 @@ app.post('/searchinduser/:email/:password',function(req,res){
 	});
 });
 
+//Package addition by a company
 app.post('/addpackage/:email/:bookdate/:no/:days/:packagetype/:cid',function(req,res){
 
 	const {email , bookdate , no ,days , packagetype , cid} = req.params;
@@ -87,9 +120,60 @@ app.post('/addpackage/:email/:bookdate/:no/:days/:packagetype/:cid',function(req
 
 });
 
+//find order details with order_id
+app.get('/order_details/:id', urlencodedParser , (req , res) => {
+		const {seller_detail , buyer_detail , product_details , name , from_place , to_place , 
+ 			   obtain_from , give_to} = req.body;
+ 		const query = {_id:req.params.id};
+ 		Order.find(query,function(err,order){
+ 			if(err) throw err;
+ 			if(order.length ==0){
+ 				console.log('No such order exists');
+ 				res.send('No such order exists');
+ 			}
+ 			if(order.length >0){
+ 				res.send(order[0]);
+ 			}
+ 		});
+});
+
+//search for tranporters in order and form transporter database
+app.get('/transporter' , (req,res) => {
+        Order.find({},function(err,data){
+			if(err) throw err;
+			// if no order is places till now
+			if(data.length ==0){
+				console.log('No order exists');
+			}
+			//if orders are there then enter their transporter details in database
+			if(data.length>0){
+				for(let k=0;k<data.length;k++){
+					const val = data[k].transporter_details;
+					const tid = val.tid;
+					const name = val.name;
+					const msgs = [{obtain_from:val.obtain_from,give_to:val.give_to}];
+					const node1 = Node({name:name,tid:tid,msgs:msgs}).save(function(err){
+						if(err) throw err;
+					});
+				}
+			}
+		});
+});
+
+
+// get transportr profile with id
+app.get('/transporter/:id' , function(req,res){
+  
+    const query = {tid:req.params.id};
+    Node.find(query, function(err,node){
+  	if(err) throw err;
+  	res.render('transporterPage',{data:node});
+  });
+});
+
+//reducing the total number after use of api
 app.get('/package/:id',function(req,res){
-	console.log('hey');
-	console.log(req.params.id);
+
 	const query = {_id : req.params.id};
 	Package.find(query,function(err,data){
 		if(err) throw err;
@@ -108,18 +192,23 @@ app.get('/package/:id',function(req,res){
 	});
 });
 
+// checking the 4 conditions
 app.post('/check/:id',urlencodedParser,function(req,res){
 
 	const pid = req.body.pid;
+	const {seller_detail , buyer_detail , product_details , name , from_place , to_place , 
+ 			   obtain_from , give_to} = req.body;
+    const order_id = (new Date()).getTime();
+    function forP(order_id);
+	const order = Order({order_id , seller_detail , buyer_detail , product_details , transporter_details :{name , from_place , to_place , 
+							 obtain_from , give_to}}).save(err => console.log(err));
 	var i=0,m=0;var errors = [],dat=[],array=[];
-    console.log('stage0');
 	const query = {_id : req.params.id};
 	Company.find(query,function(err,user){
 		console.log(req.params.id);
-		console.log('stage1');
 		if(err) throw err;
 		if(user.length == 0){
-			console.log('No such user exists');
+			console.log('No such user exists');//if no such company is already present in database
 			res.send('No such user exists');
 		}
 		if(user.length>0){
@@ -128,7 +217,7 @@ app.post('/check/:id',urlencodedParser,function(req,res){
 				console.log('stage2');
 				if(err) throw err;
 				if(usr.length == 0 ){
-					console.log('No Package Exists');
+					console.log('No Package Exists');//if the company has no package present
 					res.send('No Package Exists');
 				}
 				if(usr.length > 0){
@@ -137,10 +226,10 @@ app.post('/check/:id',urlencodedParser,function(req,res){
 						if(err) throw err;
 						if(package.length ==0){
 							console.log('No such package for this id exists');
+							//if company has packages but no package of that id exists
 							res.send('No such package for this id exists');
 						}
 						if(package.length>0){
-					    console.log('stage3');
 						for(let k = 0; k < package.length ; k++){
 
 							    const date = (new Date()).getTime();
@@ -148,16 +237,19 @@ app.post('/check/:id',urlencodedParser,function(req,res){
 								const diff = Math.floor((Math.abs(Number(date) - Number(checkdate)))/86400000);
 								if(package[k].no==0){
 									console.log('All used for package type' + package[k].packagetype);
+									//if number of apis availbale for that package is 0
 									errors[i]='All used for package type' + package[k].packagetype;
 									i++;
 								}
 								else if(diff >=  package[k].total){
 									console.log('Date of use expired for package type'  + package[k].packagetype);
+									//if package use date is expired
 									errors[i] = 'Date of use expired for package type'  + package[k].packagetype;
 									i++;
 									
 								}
 								else {
+									//if all 4 conditions are not there
 									console.log('Success');
 									dat[m] = package[k];
 									m++;
@@ -170,7 +262,117 @@ app.post('/check/:id',urlencodedParser,function(req,res){
 			}
 });}
 
-    }); });
+}); });
+
+function forP(let val){
+	var num = 0;var final = 0;
+    var value = 0;var prime = 0;var result= 0 ;var ans=0;
+    localStorage.setItem('value',0);
+
+    function toBinary(n){
+        var array=[]; var array2=[];
+        var i = 0; 
+        while (n > 0) { 
+            array[i] = n % 2; 
+            n = Math.floor(n / 2); 
+            i++; 
+        }
+        var k=0;
+        for (var j = i - 1; j >= 0; j--) {
+            array2[k] = array[j];
+            k++;
+    } 
+        return array2;
+    }
+
+    function isPrime(num){
+        var i,flag=0;
+        for(i=2; i <= num/2; i++){
+            if(num%i == 0){
+                flag = 1;
+                break;
+            }
+        }
+            if(flag==0)
+                final=num;
+            else
+                isPrime(num+1);
+                
+            return final;
+    }
+
+    function count(answer){
+        var count=0;
+        for(var m=0;m<answer.length;m++){
+            if(answer[m]==1){count++};
+        }
+        return count;
+    } 
+
+    function findPrimefactors (num) {
+        var primeFactors = [];
+        while (num % 2 === 0) {
+            primeFactors.push(2);
+            num = num / 2;
+        }
+        var sqrtNum = Math.sqrt(num);
+        for (var i = 3; i <= sqrtNum; i++) {
+            while (num % i === 0) {
+                primeFactors.push(i);
+                num = num / i;
+            }
+        }
+
+        if (num > 2) {
+            primeFactors.push(num);
+        }
+        return primeFactors;
+    }
+
+    function power(x,y,p){
+        var res = 1;
+        x=x%p;
+        while(y>0){
+            if(y & 1)
+                {res = (res*x)%p;}
+        
+        y = y/2;
+        x=(x*x)%p;
+        }
+        if(res==1)
+        return 1;
+        else
+        return 0;
+    }
+
+    function findPrimitive(n){
+        var powers=[];
+        var phi = n-1;
+        var array= findPrimefactors(phi);
+        for(var f=0;f<array.length;f++){
+        powers[f] = phi/array[f];
+        }
+        for (var r=2; r<=phi; r++) 
+        {
+            c=0;
+            for(var nn=0;nn<powers.length;nn++){
+                if(power(r, powers[nn], n) == 1 ){
+                    c=1;
+                }
+            }
+            if(c==0) return r;
+
+        }
+    }
+ 
+    var answer = toBinary(Number(val));
+    var prime = count(answer);
+    var ans = isPrime(prime);//gives P
+    localStorage.setItem('value',ans);
+    var result = findPrimitive(localStorage.getItem('value'));//gives G
+
+}
+
 
 
 app.listen('8080');
--- nodes_info.js
+++ nodes_info.js
@@ -18,6 +18,7 @@ var db = mongoose.connection;
 db.on('error', (err) => console.log(err));
 
 const TransportersSchema = new mongoose.Schema({
+	tid: {type : Number , required : true},
 	name : {type : String , required : true},
 	from_place : {type : String , required : true},
 	to_place : {type : String , required : true},
@@ -32,19 +33,23 @@ const OrderSchema = new mongoose.Schema({
     transporter_details : TransportersSchema
 });
 
+const NodeSchema = new mongoose.Schema({
+	tid : {type : Number , required : true},
+	name : {type : String , required : true},
+	msgs : {type : Array , required : true}
+});
+
 TransportersSchema.plugin(timestamp);
 OrderSchema.plugin(timestamp);
+NodeSchema.plugin(timestamp);
 
+const Node = mongoose.model('Node', NodeSchema , 'Node');
 const Order = mongoose.model('Order', OrderSchema , 'Order');
 
 db.once('open' ,() =>{
 
 	app.get('/' , (req , res) => {
-		
-		var array =[];
-	var r = Math.floor(100000000 + (Math.random()*99999999));
-	if(array.indexOf(r) === -1) array.push(r);
-    console.log(array);
+		res.render('homepage');
 	});
 
 	app.post('/add/order_details' , urlencodedParser , (req , res) => {
@@ -52,12 +57,13 @@ db.once('open' ,() =>{
  			   obtain_from , give_to} = req.body;
 		const order = Order({seller_detail , buyer_detail , product_details , transporter_details :{name , from_place , to_place , 
 							 obtain_from , give_to}}).save(err => console.log(err));
+
 	});
 
 	app.get('/order_details/:id', urlencodedParser , (req , res) => {
 		const {seller_detail , buyer_detail , product_details , name , from_place , to_place , 
  			   obtain_from , give_to} = req.body;
- 		const query = {_id:rrq.params.id};
+ 		const query = {_id:req.params.id};
  		Order.find(query,function(err,order){
  			if(err) throw err;
  			if(order.length ==0){
@@ -65,7 +71,40 @@ db.once('open' ,() =>{
  				res.send('No such order exists');
  			}
  			if(order.length >0){
- 				
+ 				res.send(order[0]);
  			}
  		});
 });
+
+app.get('/transporter' , (req,res) => {
+        Order.find({},function(err,data){
+			if(err) throw err;
+			if(data.length ==0){
+				console.log('No order exists');
+			}
+			if(data.length>0){
+				for(let k=0;k<data.length;k++){
+					const val = data[k].transporter_details;
+					const tid = val.tid;
+					const name = val.name;
+					const msgs = [{obtain_from:val.obtain_from,give_to:val.give_to}];
+					const node1 = Node({name:name,tid:tid,msgs:msgs}).save(function(err){
+						if(err) throw err;
+					});
+				}
+			}
+		});
+});
+
+
+	app.get('/transporter/:id' , function(req,res){
+      
+        const query = {tid:req.params.id};
+	    Node.find(query, function(err,node){
+      	if(err) throw err;
+      	res.render('transporterPage',{data:node});
+      });
+	});
+
+});
+
